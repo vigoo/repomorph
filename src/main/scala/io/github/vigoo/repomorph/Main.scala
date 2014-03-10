1ea58075883979efd47009531dc35d402577a555
Daniel Vigovszky <daniel.vigovszky@gmail.com>
2014-03-10 08:29:59 +0100

    Ability to turn off vcs based implementation

--- src/main/scala/io/github/vigoo/repomorph/Main.scala
+++ src/main/scala/io/github/vigoo/repomorph/Main.scala
@@ -5,13 +5,13 @@ import scala.io.Source
 import scopt.OptionParser
 import io.github.vigoo.repomorph.contexts.{MercurialMorphContext, MorphContext, FileSystemMorphContext}
 
-case class RepomorphConfig(script: File = new File("script.morph"), root: File = new File("."))
+case class RepomorphConfig(script: File = new File("script.morph"), root: File = new File("."), ignoreVCS: Boolean = false)
 
 object Main {
 
-  private def createContext(root: File): MorphContext = {
+  private def createContext(root: File, ignoreVCS: Boolean): MorphContext = {
     val hgdir = new File(root, ".hg")
-    if (hgdir.exists() && hgdir.isDirectory) {
+    if (hgdir.exists() && hgdir.isDirectory && !ignoreVCS) {
       new MercurialMorphContext(root)
     } else {
       new FileSystemMorphContext(root)
@@ -23,12 +23,13 @@ object Main {
       head("repomorph", "1.0")
       arg[File]("<script>") required() action { (x, c) => c.copy(script = x) } text("the repomorph script to be executed")
       arg[File]("<root>") required() action { (x, c) => c.copy(root = x) } text("repository root")
+      opt[Unit]("ignore-vcs") optional() action { (x, c) => c.copy(ignoreVCS = true) } text("forces repomorph to work directly on the file system")
       help("help") text("prints this help")
     }
 
     parser.parse(args, RepomorphConfig()) map { config =>
       val script = new RepomorphScript(Source.fromFile(config.script))
-      script.run(createContext(config.root))
+      script.run(createContext(config.root, config.ignoreVCS))
     }
   }
 }
